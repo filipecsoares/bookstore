b4ab5b77254c81965344fd253634a70cfae33af1
filipecsoares <filipecarlossoares@gmail.com>
2024-09-04 18:08:39 -0300

    feat: add open api documentation

--- src/main/java/com/fcs/bookstore/author/AuthorController.java
+++ src/main/java/com/fcs/bookstore/author/AuthorController.java
@@ -1,11 +1,9 @@
 package com.fcs.bookstore.author;
 
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/authors")
